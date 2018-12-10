8695609f2101fcd39aea8253305a91f86fbaecf8
Filiberto Reyes <filibertoreyes55@gmail.com>
2018-12-10 00:39:09 -0800

    first commit moving from local

---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import App from './App'
+import * as firebase from 'firebase'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vuetify from 'vuetify'
+
+// CSS
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+Vue.config.devtools = true
+Vue.use(BootstrapVue)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCY6aCtmkE4_ooUQoIeEN5b-BUMn170u6w',
+      authDomain: 'mevn-chat.firebaseapp.com',
+      databaseURL: 'https://mevn-chat.firebaseio.com',
+      projectId: 'mevn-chat',
+      storageBucket: 'mevn-chat.appspot.com',
+    })
+  }
+})
